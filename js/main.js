98b0e8f57c0905c2e2caa17313373e24c7de0e4f
itreverie <brendadhk@gmail.com>
2017-08-22 18:50:28 -0400

    Files structure

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// SCENARIO:
+//
+// This web page needs to make four AJAX calls to load its content.
+// 1. These AJAX calls should all happen asynchronously and concurrently.
+// 2. Each time an AJAX calls completes, update the progress bar.
+// 3. When all four AJAX calls complete, display the content
+//    for each box.
+//
+// Box one:   https://jstest.getsandbox.com/one
+// Box two:   https://jstest.getsandbox.com/two
+// Box three: https://jstest.getsandbox.com/three
+// Box four:  https://jstest.getsandbox.com/four
+//
+// The AJAX returns the following JSON structure: {"content": string}
+
+
+
