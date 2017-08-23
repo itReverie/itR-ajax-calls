5148b794055ff0bb46dadffbb8bce588c14eba61
itreverie <brendadhk@gmail.com>
2017-08-22 21:32:24 -0400

    Adding an asynchronous call

--- js/main.js
+++ js/main.js
@@ -25,36 +25,42 @@ function renderResults() {
     dictionary.push({key: 3, value: "three"});
     dictionary.push({key: 4, value: "four"});
 
-    for (let i = 0; i < dictionary.length; i++) {
+    for (let i = 0; i < 1; i++) {
         //console.log(dictionary[i].key+' '+ dictionary[i].value);
         makeRequest('box ' + dictionary[i].value, 'http://www.coincap.io/coins');//'https://jstest.getsandbox.com/' + dictionary[i].key);
     }
 }
 
+function reqListener() {
+    var resp = this.responseText;
+    var respToString = JSON.stringify(resp);
+    //RESULT in Json
+    var respTojson = JSON.parse(respToString);
+    console.log(respTojson);
+}
 
 function makeRequest(box, url) {
 
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
-    request.setRequestHeader('Content-Type', 'application/json');
+    //request.setRequestHeader('Content-Type', 'application/json');
 
-    //Assynchronous not working
-    // request.onload = function () {
+    //Assynchronous
+     request.onload = reqListener;
+    //request.addEventListener("load", reqListener);
 
     //Synchronous Working
-    request.onreadystatechange = function() {
-        console.log(request.status);
-            if (request.status >= 200 && request.status < 400) {
-                // Success!
-                var resp = request.responseText;
-                console.log(resp);
-                var toRender = JSON.parse(resp);
-                console.log(toRender);
-            } else {
-                // We reached our target server, but it returned an error
-
-            }
-        };
+    // request.onreadystatechange = function () {
+    //     if (request.status >= 200 && request.status < 400) {
+    //         // Success!
+    //         var resp = request.responseText;
+    //         var respToString = JSON.stringify(resp);
+    //         //RESULT in Json
+    //         var respTojson = JSON.parse(respToString);
+    //     } else {
+    //         console.log('Error');
+    //     }
+    // };
 
     request.onerror = function () {
         console.log(request.statusText);
